e838b8e9f5bb6a5acd26ae8d33809f2b1167fa73
yebin <yebind3308@gmail.com>
2023-12-11 10:31:41 +0900

    fix: Card 중복 오류 수정

--- src/containers/CardWrap.tsx
+++ src/containers/CardWrap.tsx
@@ -8,38 +8,39 @@ import { styled } from "@mui/system";
 
 const CardWrap = () => {
   return (
-    <Box
-      sx={{
-        width: "100%",
-        padding: "2rem 0",
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fill, minmax(15rem, 1fr))",
-        rowGap: "2rem",
-        columnGap: "2rem",
-      }}
-    >
-      {data.map((value, index) => (
-        <Box
-          key={index}
-          sx={{
-            display: "none",
-            width: "100%",
-            border: "1px solid lightgray",
-            borderRadius: "5px",
-            rowGap: "4",
-          }}
-        >
-          <CardImg img={value.img} />
-          <CardText
-            location={value.location}
-            score={value.score}
-            view={value.view}
-            day={value.day}
-            price={value.price}
-          />
-        </Box>
-      ))}
-    </Box>
+    <>
+      <Box
+        sx={{
+          width: "100%",
+          padding: "2rem 0",
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(15rem, 1fr))",
+          rowGap: "2rem",
+          columnGap: "2rem",
+        }}
+      >
+        {data.map((value, index) => (
+          <Box
+            key={index}
+            sx={{
+              width: "100%",
+              border: "1px solid lightgray",
+              borderRadius: "5px",
+              rowGap: "4",
+            }}
+          >
+            <CardImg img={value.img} />
+            <CardText
+              location={value.location}
+              score={value.score}
+              view={value.view}
+              day={value.day}
+              price={value.price}
+            />
+          </Box>
+        ))}
+      </Box>
+    </>
   );
 };
 
