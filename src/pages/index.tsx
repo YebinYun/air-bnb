a5d58987512887a49671208bfbd5a463a0c4a913
yebin <yebind3308@gmail.com>
2023-12-18 14:55:14 +0900

    feat: recoil 설치

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import UserLayout from "@/layout/UserLayout";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { RecoilRoot } from "recoil";
 
 const queryClient = new QueryClient();
 
 export default function Home() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <UserLayout />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <UserLayout />
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 }
