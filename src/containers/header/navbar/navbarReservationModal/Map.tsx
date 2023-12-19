ed97779be8f535a6f53069b507053574d9bb9d81
yebin <yebind3308@gmail.com>
2023-12-19 18:02:38 +0900

    feat: world-countries install

--- src/containers/header/navbar/navbarReservationModal/Map.tsx
+++ src/containers/header/navbar/navbarReservationModal/Map.tsx
@@ -1,24 +1,26 @@
 import React from "react";
-import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { divIcon } from "leaflet";
 import "leaflet/dist/leaflet.css";
-import dynamic from "next/dynamic";
 
 // world country 설치  그담에 react-select library 설치해서 select option 값으로 country 를 고를수 있게 하고, 고르면 해당 좌표로 이동!!
 
 const Map = ({ lat, lng }) => {
+  const ICON = divIcon();
+
   return (
-    <MapContainer
-      style={{ width: "100%", height: "450px" }}
-      center={[lat, lng]}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[lat, lng]} />
-    </MapContainer>
+    <>
+      <MapContainer
+        key={lat}
+        style={{ width: "100%", height: "400px", margin: "2rem 0 " }}
+        center={[lat, lng]}
+        zoom={6}
+        scrollWheelZoom={false}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker icon={ICON} position={[lat, lng]} />
+      </MapContainer>
+    </>
   );
 };
 
--- src/containers/header/navbar/navbarReservationModal/ReservationMapSearch.tsx
+++ src/containers/header/navbar/navbarReservationModal/ReservationMapSearch.tsx
@@ -1,21 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import dynamic from "next/dynamic";
+import { Select } from "@mui/material";
+import countries from "world-countries";
+import { map } from "leaflet";
 
 const Map = dynamic(() => import("./Map"), {
   ssr: false,
 });
 
+console.log("@countries:", countries);
+
 const ReservationMapSearch = () => {
   const [coords, setCoords] = useState({
-    lat: "",
-    lng: "",
+    lat: 37.5761827,
+    lng: 126.8998998,
   });
 
   useEffect(() => {
     const watchId = navigator.geolocation.watchPosition((position) => {
       const { latitude, longitude } = position.coords;
-      // Show a map centered at latitude / longitude.
-
       setCoords({
         lat: latitude,
         lng: longitude,
@@ -23,10 +26,30 @@ const ReservationMapSearch = () => {
     });
   }, []);
 
+  console.log("##coords:", coords);
+  const onChangeHandler = (e) => {
+    console.log("e", e);
+    const lat = e.target.value.split(",")[0];
+    const lng = e.target.value.split(",")[1];
+    setCoords({
+      lat: Number(lat),
+      lng: Number(lng),
+    });
+  };
+
   return (
-    <div>
-      <Map lat={coords.lat} lng={coords.lng} />
-    </div>
+    <>
+      <select
+        onChange={(e) => {
+          onChangeHandler(e);
+        }}
+      >
+        {countries?.map((x) => (
+          <option value={x?.latlng}>{x?.name?.common}</option>
+        ))}
+      </select>
+      <Map lat={Number(coords.lat)} lng={Number(coords.lng)} />
+    </>
   );
 };
 
