c7d71f0b6c0f234acb02eaf9d922d7d39f2f430b
yebin <yebind3308@gmail.com>
2023-12-23 17:13:41 +0900

    chore: recoil install

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+"use client";
 import HeaderComponent from "@/components/header/HeaderComponent";
 import React from "react";
+import { RecoilRoot } from "recoil";
 
 const page = () => {
   return (
-    <>
+    <RecoilRoot>
       <HeaderComponent />
-    </>
+    </RecoilRoot>
   );
 };
 
