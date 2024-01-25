f03ec3263bcd6bb5bfd9d5288ce0defafc2f573a
yebin <yebind3308@gmail.com>
2024-01-25 18:39:25 +0900

    Bug Fix

--- src/containers/header/HeaderContainer.tsx
+++ src/containers/header/HeaderContainer.tsx
@@ -1,16 +1,25 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import HeaderTopContainer from "./HeaderTopContainer";
 import HeaderBottomContainer from "./HeaderBottomContainer";
 import { useSetRecoilState } from "recoil";
 import { userDataSelector } from "@/store/auth/userData";
 
 const HeaderContainer = () => {
+  const [tokenData, setTokenData] = useState<any>();
   const setUserData = useSetRecoilState(userDataSelector);
-  const tokenData: any =
-    typeof window !== undefined && localStorage.getItem("token");
+
+  // const tokenData: any =
+  //   typeof window !== undefined && localStorage.getItem("token");
 
   useEffect(() => {
+    if (typeof window !== undefined) {
+      const data = localStorage.getItem("token");
+      setTokenData(data);
+    } else {
+      return undefined;
+    }
+
     const getUserInfo = () => {
       if (tokenData) {
         return setUserData(JSON.parse(tokenData));
