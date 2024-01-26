69f73f91302e017e9cd8d9908e857492cd8bf8d0
yebin <yebind3308@gmail.com>
2024-01-26 17:53:02 +0900

    err: Mixed Content Error

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ["localhost", "*", "cf.bstatic.com", "a0.muscache.com"],
   },
+  rewrites: () => [
+    {
+      source: "/post",
+      destination: "https://next13-hotels-reservation-client.vercel.app/",
+    },
+  ],
   reactStrictMode: true,
 };
 
