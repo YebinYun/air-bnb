edf2e7cd886f7ccaa9d41edd9a0cc690b9987058
yebin <yebind3308@gmail.com>
2024-01-29 10:46:49 +0900

    fix: head add http -> https

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      http-equiv="Content-Security-Policy"
+      content="upgrade-insecure-requests"
+    >
       <body className={inter.className}>{children}</body>
     </html>
   );
